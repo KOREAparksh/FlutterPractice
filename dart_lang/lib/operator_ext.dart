6e2b2663a35a83bf8553faae8fc4df753dc7a57f
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2020-07-11 22:52:23 +0900

    [practice]

--- dart_lang/lib/operator_ext.dart
+++ dart_lang/lib/operator_ext.dart
@@ -1,1 +1,23 @@
-void main() {}
+import 'dart:convert';
+import 'dart:io';
+
+void main() {
+  String jsonStr = """
+  {
+    "basket" : {
+      "apple" : 50,
+      "banana" : 10,
+      "grape" : 5
+    }
+  }
+  """;
+
+  Map json = jsonDecode(jsonStr);
+  print('$json');
+
+  Map basket = json["basket"];
+  print('$basket');
+
+  basket.update('apple', (value) => 1000);
+  print('$basket');
+}
